b9de1ab4f4d46b46285cb1a5a9ccc6d30fde6cbf
dliyaaul <bakun8580@gmail.com>
2022-10-31 13:32:35 +0700

    master

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -92,7 +92,7 @@
 
     <!--Footer-->
     <footer class="bg-primary text-white text-center p-3">
-        <p>Made with ❤ by <a href="#" class="text-white fw-bold">Alexander Sebastian Richard</a></p>
+        <p>Made with ❤ by <a href="#" class="text-white fw-bold">Muhammad Dliyaa'ul Haq Shidqey</a></p>
     </footer>
     <!--End of footer-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -78,14 +78,14 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{ asset('template/vendor/jquery/jquery.min.js')}}"></script>
-    <script src="{{ asset('template/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="{{ asset('template/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="{{ asset('template/js/sb-admin-2.min.js')}}"></script>
+    <script src="js/sb-admin-2.min.js"></script>
 
 </body>
 
