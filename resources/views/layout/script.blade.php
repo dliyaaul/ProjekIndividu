73030ef615a40801f5dc83ba4788279bcfda776d
dliyaaul <bakun8580@gmail.com>
2022-11-03 13:40:36 +0700

    update

--- resources/views/layout/script.blade.php
+++ resources/views/layout/script.blade.php
@@ -25,11 +25,11 @@
 </div>
 
 <!-- Bootstrap core JavaScript-->
-<script src="template/vendor/jquery/jquery.min.js"></script>
-<script src="template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ asset('template/vendor/jquery/jquery.min.js')}}"></script>
+<script src="{{ asset('template/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
 <!-- Core plugin JavaScript-->
-<script src="template/vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="{{ asset('template/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
 
 <!-- Custom scripts for all pages-->
-<script src="template/js/sb-admin-2.min.js"></script>
+<script src="{{ asset('template/js/sb-admin-2.min.js')}}"></script>
